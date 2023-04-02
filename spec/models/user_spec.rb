ea9f2644559d8185d00faaa791a8ea4aa82620c9
SAZAMUGAN <103872710+SAZAMUGAN@users.noreply.github.com>
2023-04-02 20:38:14 +0900

    FactoryBotやテストコードの記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+
   describe 'ユーザー新規登録' do
     it 'nicnameが空では登録できない' do
-      user = User.new(nickname: '', email: 'test@example', password: '123456', password_confirmation: '123456')
-      user.valid?
-      expect(user.errors.full_messages).to include("Nickname can't be blank")
+      @user.nickname = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
     end
     it 'emailが空では登録できない' do
-      user = User.new(nickname: 'Mike', email: '', password: '123456', password_confirmation: '123456')
-      user.valid?
-      expect(user.errors.full_messages).to include("Email can't be blank")
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
     end
   end
 end
